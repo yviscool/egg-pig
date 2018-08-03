baf6d3e99103e7464dac4eddb4df18c32ea217f8
Y.V <5196666qwe@gmail.com>
2018-08-03 23:48:09 +0800

    refactor:

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 const common = require('./lib/common');
 const decorator = require('./lib/decorator');
-const MiddlewareConsumer = require('./lib/middleware');
 const HttpStatus = require('./lib/exceptions/constant');
 const HttpException = require('./lib/exceptions/exception');
+const MiddlewareConsumer = require('./lib/middleware');
 
 module.exports = {
   ...common,
